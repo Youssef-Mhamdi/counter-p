075a98a190438d6d2b34d2f7707eea3bf3564ef5
Youssef Mhamdi <wp.blackyoussef@gmail.com>
2021-06-19 13:45:51 +0100

    First commit, all good.

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,29 @@
 import { createStore } from 'vuex'
+import axios from 'axios';  
+
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        counter: 0
+    },
+    mutations: {
+        increasecounter(state, randomN) {
+            state.counter+= randomN
+        },
+        decreasecounter(state, x) {
+            state.counter > 0 ? state.counter -= x : console.log(" Only positive numbers... ");
+        }
+    },
+    actions: {
+        increasecounter( {commit} ) {
+            axios('https://www.random.org/integers/?num=1&min=1&max=9&col=1&base=10&format=plain&rnd=new').then( result =>{
+                commit('increasecounter', result.data ? result.data : 'No date from API' )
+            })
+        },
+        decreasecounter( {commit} ) {
+            const x = 1
+            commit('decreasecounter', x)
+        }
+    },
+    modules: {}
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<div class="counter"> {{ $store.state.counter }} </div>
+		<div class="counterSquared"> 
+			<span>{{$store.state.counter}} <sup>2</sup> = 
+			{{ $store.state.counter * $store.state.counter }} 
+			</span>
+		</div>
+
+		<div>
+			<button @click="$store.dispatch('decreasecounter')">-</button>
+			<button @click="$store.dispatch('increasecounter')">+</button>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'Home',
+	}
 </script>
+
+<style scoped>
+	button {
+		font-size: 24px;
+		margin: 5px;
+		padding: 10px 20px;
+		width: 100px;
+	}
+
+	div.counter {
+		font-size: 24px;
+	}
+</style>
